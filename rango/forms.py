3c607631b7e656b2385be5a39886dbb5d5e617c1
Douglas Ormrod <3019255O@student.gla.ac.uk>
2026-02-12 14:51:52 +0000

    chatper 7 commit

---
+++ rango/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from rango.models import Category, Page
+
+class CategoryForm(forms.ModelForm):
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    slug = forms.CharField(widget=forms.HiddenInput(), required=False)
+
+    class Meta:
+        model = Category
+        fields = ('name',)
+        help_texts = {
+            'name' : "Please enter the category name."
+        }
+
+
+class PageForm(forms.ModelForm):
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+
+    class Meta:
+        model = Page
+        exclude = ('category',)
+        help_texts = {
+            'title' : "Please enter the title of the page.",
+            'url' : "Please enter the URL of the page.",
+        }
